0ca1ff34a1da3c344172699d46c7839aa089c8cd
Srinivas Kondepudi <ksrinivas07530@gmail.com>
2026-04-11 09:23:18 +0200

    Add GitHub Actions CI/CD for Railway, remove dead orchestrator code
    - ci.yml: compile + flake8 errors on backend, lint + build on frontend
    - deploy.yml: railway up --service kubeopt on push to main
    - Remove application/use_cases/ (orchestrator + broken generator imports,
      741 lines of dead code from Oct 2025 cleanup that was never used)

--- application/use_cases/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .core import OptimizationConfig, ExecutableCommand, ComprehensiveExecutionPlan
-
-__all__ = [
-    'OptimizationConfig',
-    'ExecutableCommand', 
-    'ComprehensiveExecutionPlan'
-]
